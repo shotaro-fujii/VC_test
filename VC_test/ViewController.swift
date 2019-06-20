4daeea92777b40466ead7df089b8e4b21f7287ba
shotaro-fujii <shotaro@kmd.keio.ac.jp>
2019-06-20 11:44:17 +0900

    Initial Commit

---
+++ VC_test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VC_test
+//
+//  Created by 藤井翔大朗 on 2019/06/20.
+//  Copyright © 2019 藤井翔大朗. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
