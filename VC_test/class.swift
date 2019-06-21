7679e1c01b906cc38f123e1dd118e17453268f03
shotaro-fujii <shotaro@kmd.keio.ac.jp>
2019-06-21 09:54:47 +0900

    受け渡し用データのクラスを作成

---
+++ VC_test/class.swift
@@ -0,0 +1,20 @@
+//
+//  class.swift
+//  VC_test
+//
+//  Created by 藤井翔大朗 on 2019/06/21.
+//  Copyright © 2019 藤井翔大朗. All rights reserved.
+//
+
+import Foundation
+
+
+
+
+class DataClass {
+    
+    //定数の定義
+    let str1 = ""
+    let str2 = ""
+    
+}
